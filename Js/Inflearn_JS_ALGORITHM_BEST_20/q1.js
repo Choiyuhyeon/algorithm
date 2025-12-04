6ccfceb2469231edb1571916507534ebc6a39903
Haning <100738360+Choiyuhyeon@users.noreply.github.com>
2025-12-05 03:44:07 +0900

    feat: 실수고치기

--- Js/Inflearn_JS_ALGORITHM_BEST_20/q1.js
+++ Js/Inflearn_JS_ALGORITHM_BEST_20/q1.js
@@ -1,28 +1,42 @@
-function solution(data) {
-    let answer = "";
+// function solution(data) {
+//     let answer = "";
 
-    // data: [' + - - + - + - ', ' + + + - + - + ', ' + + - + + + - ']
-    for (let i = 0; i < data.length; i++) {
-        const seg = data[i];   // 현재 문자열
-        let bits = "";
+//     // data: [' + - - + - + - ', ' + + + - + - + ', ' + + - + + + - ']
+//     for (let i = 0; i < data.length; i++) {
+//         const seg = data[i];   // 현재 문자열
+//         let bits = "";
 
-        // seg 안에서 + / - 만 뽑아서 1/0으로 변환
-        for (let j = 0; j < seg.length; j++) {
-            let ch = seg[j];
-            if (ch === "+") {
-                bits += "1";
-            } else if (ch === "-") {
-                bits += "0";
-            }
-            // 공백은 무시
-        }
+//         // seg 안에서 + / - 만 뽑아서 1/0으로 변환
+//         for (let j = 0; j < seg.length; j++) {
+//             let ch = seg[j];
+//             if (ch === "+") {
+//                 bits += "1";
+//             } else if (ch === "-") {
+//                 bits += "0";
+//             }
+//             // 공백은 무시
+//         }
 
-        // 2진수 → 10진수 → ASCII 문자
-        const num = parseInt(bits, 2);
-        const asciiChar = String.fromCharCode(num);
-        answer += asciiChar;
+//         // 2진수 → 10진수 → ASCII 문자
+//         const num = parseInt(bits, 2);
+//         const asciiChar = String.fromCharCode(num);
+//         answer += asciiChar;
+//     }
+
+//     return answer;
+// }
+// // try 1
+
+
+// data: [' + - - + - + - ', ' + + + - + - + ', ' + + - + + + - ']
+function solution(data){
+    let chararr;
+    let bits;
+
+    for(let i of data){
+        let pmarr;
+        for(let j of i){
+            if(j=='+') {bits += '1';}
+            else if(j=='-') {bits += '0';}
     }
-
-    return answer;
-}
-// try 1
+}뭐하고있는거임
